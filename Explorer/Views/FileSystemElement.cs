3b0a228ad34d1ae05aa1fccb92857df93644770a
junkkerrigan <ander4ik@ukr.net>
2019-10-10 17:25:32 +0300

    implemented some IFileSystemElement methods

---
+++ Explorer/Views/FileSystemElement.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Explorer.Presenters;
+
+namespace Explorer.Views
+{
+    public abstract class FileSystemElement : IFileSystemElement
+    {
+        public string Path { get; set; }
+
+        void IFileSystemElement.CopyTo(string destinationPath)
+        {
+            Copy(this.Path, destinationPath);
+        }
+
+        protected abstract void Copy(string sourcePath, string destinationPath);
+
+        public abstract void Delete();
+
+        public abstract void EditName(string newName);
+
+        public abstract void OpenWithDefaultApplication();
+
+        void IFileSystemElement.Move(string destinationPath)
+        {
+
+        }
+    }
+
+    public class DirectoryElement : FileSystemElement
+    {
+        protected override void Copy(string sourcePath, string destinationPath)
+        {
+            if (Directory.Exists(destinationPath))
+            {
+                throw new DirectoryAlreadyExistsException();
+            }
+            Directory.CreateDirectory(destinationPath);
+
+            string[] subDirs = Directory.GetDirectories(sourcePath);
+            foreach (string dir in subDirs)
+            {
+                string folderName = dir.Substring(dir.LastIndexOf('\\') + 1),
+                    newDestPath = System.IO.Path.Combine(destinationPath, folderName);
+                Copy(dir, newDestPath);
+            }
+
+            string[] innerFiles = Directory.GetFiles(sourcePath);
+            foreach (string file in innerFiles)
+            {
+                string fileName = System.IO.Path.GetFileName(file),
+                    destFileName = System.IO.Path.Combine(destinationPath, fileName);
+                File.Copy(file, destFileName);
+            }
+
+            // ShowModalWhenFinished();
+        }
+
+        public override void Delete()
+        {
+            Directory.Delete(this.Path, true);
+        }
+
+        public override void EditName(string newName)
+        {
+            string newPath = this.Path.Remove(this.Path.LastIndexOf('\\') + 1) + newName;
+            Directory.Move(this.Path, newPath);
+        }
+    }
+
+    public class FileElement : FileSystemElement
+    {
+        protected override void Copy(string sourcePath, string destinationPath)
+        {
+            if (File.Exists(destinationPath))
+            {
+                throw new FileAlreadyExistsException();
+            }
+            File.Copy(sourcePath, destinationPath);
+
+            // ShowModalWhenFinished();
+        }
+
+        public override void Delete()
+        {
+            File.Delete(this.Path);
+        }
+
+        public override void EditName(string newName)
+        {
+            string newPath = this.Path.Remove(this.Path.LastIndexOf('\\') + 1) + newName;
+            File.Move(this.Path, newPath);
+        }
+    }
+}
